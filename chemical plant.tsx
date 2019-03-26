b4a1baaf81d0712593f0c8863c9f8f35dfdf73d0
LukluGames <36222233+LukluGames@users.noreply.github.com>
2019-03-26 12:43:52 -0400

    Add files via upload

---
+++ chemical plant.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Chemical plant zone tilesheet" tilewidth="129" tileheight="129" spacing="1" tilecount="320" columns="10">
+ <image source="Chemical plant zone tilesheet.png" trans="ff00ff" width="1302" height="4282"/>
+</tileset>
